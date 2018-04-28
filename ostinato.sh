d42d0fb37c87f4c762431c812d786fbeac0a8923
Rama Sasthri, Kristipati <Rama.Kristipati@gmail.com>
2018-04-28 11:06:06 +0530

    ostinato

---
+++ ostinato.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install -y qt4-qmake
+sudo apt-get install -y libqt4-dev
+sudo apt install -y make
+sudo apt  install -y protobuf-compiler
+sudo apt install -y g++
+sudo apt-get install -y libprotobuf-dev
+sudo apt-get install -y libpcap-dev
